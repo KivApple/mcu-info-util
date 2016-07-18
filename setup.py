8ad2f50b379a25168c535b80f008cec93ab0d074
kiv <kiv.apple@gmail.com>
2016-07-19 03:48:07 +0400

    ...

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 import os
 from setuptools import setup
 
+metadata_files = []
+for d, folders, files in os.walk('share'):
+	for f in files:
+		metadata_files.append(os.path.join(d, f))
+
 setup(
 	name='mcu-info-util',
 	version='0.1',
@@ -9,7 +14,7 @@ setup(
 	url='https://github.com/KivApple/mcu-info-util',
 	packages=['mcu_info_util'],
 	package_dir={'mcu-info-util': 'mcu-info-util'},
-	package_data=[(d, [os.path.join(d, f) for f in files]) for d, folders, files in os.walk('share')],
+	package_data={'mcu-info-util': metadata_files},
 	entry_points={
 		'console_scripts': [
 			'mcu-info-util = mcu_info_util.__main__:main'
