ad461c648b41d91524a53108d09c3a58432eafa6
kiv <kiv.apple@gmail.com>
2016-07-19 03:34:42 +0400

    ...

--- setup.py
+++ setup.py
@@ -8,12 +8,14 @@ setup(
 	author_email='kiv.apple@gmail.com',
 	url='https://github.com/KivApple/mcu-info-util',
 	packages=['mcu_info_util'],
+	package_dir={'mcu-info-util': 'mcu-info-util'},
+	package_data={'mcu-info-util': (d, [os.path.join(d, f) for f in files]) for d, folders, files in os.walk('share')},
 	entry_points={
 		'console_scripts': [
 			'mcu-info-util = mcu_info_util.__main__:main'
 		]
 	},
-	data_files=[
-		(d, [os.path.join(d, f) for f in files]) for d, folders, files in os.walk('share')
-	]
+	#data_files=[
+	#	(d, [os.path.join(d, f) for f in files]) for d, folders, files in os.walk('share')
+	#]
 )
