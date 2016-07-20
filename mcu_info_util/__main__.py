a981c766817c70fc515325549558ca47321ad8f0
kiv <kiv.apple@gmail.com>
2016-07-20 06:50:51 +0400

    --print-flags option

--- mcu_info_util/__main__.py
+++ mcu_info_util/__main__.py
@@ -1,13 +1,15 @@
 import argparse
 import os
 
-import mcu_info_util.linker_script
+from mcu_info_util import linker_script
 from mcu_info_util.device_tree import DeviceTree
 from mcu_info_util.svd import load_svd_for_mcu
 
 def main():
 	parser = argparse.ArgumentParser(description='MCU info util')
 	parser.add_argument('--mcu', help='specify MCU model (e. g. STM32F407VG)')
+	parser.add_argument('--print-flags', help='print required compiler and linker flags for target MCU',
+						action='store_true')
 	parser.add_argument('--linker-script', help='specify output file name for the linker script')
 	parser.add_argument('--header', help='specify output file name for the C/C++ header')
 	parser.add_argument('--use-defines', help='use #define instead of static const', action='store_true')
@@ -26,8 +28,9 @@ def main():
 		device_tree = DeviceTree(metadata_dir + '/devices.data')
 		entry = device_tree.find(args.mcu)
 		compiler_options = entry.get_options(enable_libopencm3=args.enable_libopencm3)
-		compiler_options_str = ' '.join(compiler_options)
-		print(compiler_options_str)
+		if args.print_flags:
+			compiler_options_str = ' '.join(compiler_options)
+			print(compiler_options_str)
 		if args.linker_script:
 			linker_script.generate(args.linker_script, compiler_options)
 		if args.header:
