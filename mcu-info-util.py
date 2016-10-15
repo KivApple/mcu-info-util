7e31aab04dbac75709068db6f148240538486442
Ivan Kolesnikov <kiv.apple@gmail.com>
2016-10-15 12:11:45 +0400

    Allow to run mcu-info-util.py without installation from any dir

--- mcu-info-util.py
+++ mcu-info-util.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
+import os, sys
+cur_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(cur_dir)
 from mcu_info_util.__main__ import main
+
 
 if __name__ == "__main__":
 	main()
