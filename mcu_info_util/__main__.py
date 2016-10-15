17deb0ab358fb9c8e57affbe40bde8d62c4cffc8
Ivan Kolesnikov <kiv.apple@gmail.com>
2016-10-15 10:39:17 +0400

    Find compiler support AVR support

--- mcu_info_util/__main__.py
+++ mcu_info_util/__main__.py
@@ -2,43 +2,47 @@ import argparse
 import os
 
 from mcu_info_util import linker_script
-from mcu_info_util.device_tree import DeviceTree
 from mcu_info_util.svd import load_svd_for_mcu
+from mcu_info_util.find_compiler import find_compiler, find_compiler_options
+
 
 def main():
-	parser = argparse.ArgumentParser(description='MCU info util')
-	parser.add_argument('--mcu', help='specify MCU model (e. g. STM32F407VG)')
-	parser.add_argument('--print-flags', help='print required compiler and linker flags for target MCU',
-						action='store_true')
-	parser.add_argument('--linker-script', help='specify output file name for the linker script')
-	parser.add_argument('--header', help='specify output file name for the C/C++ header')
-	parser.add_argument('--use-defines', help='use #define instead of static const', action='store_true')
-	parser.add_argument('--enable-libopencm3', help='enable linking with libopencm3 library (if supported for target MCU)',
-						action='store_true')
-	parser.add_argument('--metadata-dir', help='specify metadata directory')
-	args = parser.parse_args()
+    parser = argparse.ArgumentParser(description='MCU info util')
+    parser.add_argument('--mcu', help='specify MCU model (e. g. STM32F407VG)')
+    parser.add_argument('--find-compiler', help='search target MCU toolchain and print path to the compiler executable',
+                        action='store_true')
+    parser.add_argument('--print-flags', help='print required compiler and linker flags for target MCU',
+                        action='store_true')
+    parser.add_argument('--linker-script', help='specify output file name for the linker script')
+    parser.add_argument('--header', help='specify output file name for the C/C++ header')
+    parser.add_argument('--use-defines', help='use #define instead of static const', action='store_true')
+    parser.add_argument('--enable-libopencm3', help='enable linking with libopencm3 library (if supported for target MCU)',
+                        action='store_true')
+    parser.add_argument('--metadata-dir', help='specify metadata directory')
+    args = parser.parse_args()
 
-	if args.metadata_dir:
-		metadata_dir = args.metadata_dir
-	else:
+    if args.metadata_dir:
+        metadata_dir = args.metadata_dir
+    else:
 
-		metadata_dir = os.path.join(os.path.dirname(__file__), 'metadata')
+        metadata_dir = os.path.join(os.path.dirname(__file__), 'metadata')
 
-	if args.mcu:
-		device_tree = DeviceTree(metadata_dir + '/devices.data')
-		entry = device_tree.find(args.mcu)
-		compiler_options = entry.get_options(enable_libopencm3=args.enable_libopencm3)
-		if args.print_flags:
-			compiler_options_str = ' '.join(compiler_options)
-			print(compiler_options_str)
-		if args.linker_script:
-			linker_script.generate(args.linker_script, compiler_options)
-		if args.header:
-			svd = load_svd_for_mcu(metadata_dir + '/SVD', args.mcu)
-			if svd:
-				svd.generate_header(args.header, use_defines=args.use_defines)
-	else:
-		parser.print_help()
+    if args.mcu:
+        compiler_executable = find_compiler(args.mcu, metadata_dir, args)
+        compiler_options = find_compiler_options(args.mcu, metadata_dir, args)
+        if args.find_compiler:
+            print(compiler_executable)
+        if args.print_flags:
+            compiler_options_str = ' '.join(compiler_options)
+            print(compiler_options_str)
+        if args.linker_script:
+            linker_script.generate(args.linker_script, compiler_options)
+        if args.header:
+            svd = load_svd_for_mcu(metadata_dir + '/SVD', args.mcu)
+            if svd:
+                svd.generate_header(args.header, use_defines=args.use_defines)
+    else:
+        parser.print_help()
 
 if __name__ == "__main__":
-	main()
+    main()
--- mcu_info_util/linker_script.py
+++ mcu_info_util/linker_script.py
@@ -1,103 +1,109 @@
 from six import iteritems
 
+
 def generate(file_name, options):
-	defines = {}
-	for option in options:
-		if not option.startswith('-D_'):
-			continue
-		parts = option.split('=', 1)
-		if len(parts) < 2:
-			continue
-		name = parts[0][3:]
-		value = parts[1]
-		defines[name] = value
-	f = open(file_name, 'w')
-	f.write('EXTERN(vector_table)\n')
-	f.write('ENTRY(reset_handler)\n')
-	f.write('\n')
-	f.write('MEMORY {\n')
-	for k, v in iteritems(defines):
-		if k.endswith('_OFF'):
-			continue
-		offset_key = '%s_OFF' % (k)
-		if not (offset_key in defines):
-			continue
-		offset = defines[offset_key]
-		length = v
-		if k.startswith('ROM'):
-			access = 'rx'
-		elif k == 'EEP':
-			access = 'r'
-		else:
-			access = 'rwx'
-		f.write('\t%s (%s) : ORIGIN = %s, LENGTH = %s\n' % (k.lower(), access, offset, length))
-	f.write('}\n')
-	f.write('\n')
-	f.write('SECTIONS {\n')
-	f.write('\t.text : {\n')
-	f.write('\t\t*(.vectors)\n')
-	f.write('\t\t*(.text)\n')
-	f.write('\t\t. = ALIGN(4);\n')
-	f.write('\t\t*(.rodata)\n')
-	f.write('\t\t. = ALIGN(4);\n')
-	f.write('\t} >rom\n')
-	f.write('\t.preinit_array : {\n')
-	f.write('\t\t__preinit_array_start = .;\n')
-	f.write('\t\tKEEP (*(.preinit_array))\n')
-	f.write('\t\t__preinit_array_end = .;\n')
-	f.write('\t} >rom\n')
-	f.write('\t.init_array : {\n')
-	f.write('\t\t__init_array_start = .;\n')
-	f.write('\t\tKEEP (*(SORT(.init_array.*)))\n')
-	f.write('\t\tKEEP (*(.init_array))\n')
-	f.write('\t\t__init_array_end = .;\n')
-	f.write('\t} >rom\n')
-	f.write('\t.fini_array : {\n')
-	f.write('\t\t__fini_array_start = .;\n')
-	f.write('\t\tKEEP (*(.fini_array))\n')
-	f.write('\t\tKEEP (*(SORT(.fini_array.*)))\n')
-	f.write('\t\t__fini_array_end = .;\n')
-	f.write('\t} >rom\n')
-	f.write('\t.ARM.extab : {\n')
-	f.write('\t\t*(.ARM.extab*)\n')
-	f.write('\t} >rom\n')
-	f.write('\t.ARM.exidx : {\n')
-	f.write('\t\t__exidx_start = .;\n')
-	f.write('\t\t*(.ARM.exidx*)\n')
-	f.write('\t\t__exidx_end = .;\n')
-	f.write('\t} >rom\n')
-	f.write('\t. = ALIGN(4);\n')
-	f.write('\t_etext = .;\n')
-	f.write('\t.data : {\n')
-	f.write('\t\t_data = .;\n')
-	f.write('\t\t*(.data*)\n')
-	f.write('\t\t. = ALIGN(4);\n')
-	f.write('\t\t_edata = .;\n')
-	f.write('\t} >ram AT >rom\n')
-	f.write('\t_data_loadaddr = LOADADDR(.data);\n')
-	f.write('\t.bss : {\n')
-	f.write('\t\t*(.bss*)\n')
-	f.write('\t\t*(COMMON)\n')
-	f.write('\t\t. = ALIGN(4);\n')
-	f.write('\t\t_ebss = .;\n')
-	f.write('\t} >ram\n')
-	for k, v in iteritems(defines):
-		if k.endswith('_OFF'):
-			continue
-		offset_key = '%s_OFF' % (k)
-		if not (offset_key in defines):
-			continue
-		if k in ('RAM', 'ROM'):
-			continue
-		name = k.lower()
-		f.write('\t.%s : {\n' % (name))
-		f.write('\t\t*(.%s*)\n' % (name))
-		f.write('\t\t. = ALIGN(4);\n')
-		f.write('\t} >%s\n' % (name))
-	f.write('\t/DISCARD/ : { *(.eh_frame) }\n')
-	f.write('\t. = ALIGN(4);\n')
-	f.write('\tend = .;\n')
-	f.write('}\n')
-	f.write('\n')
-	f.write('PROVIDE(_stack = ORIGIN(ram) + LENGTH(ram));\n')
-	f.close()
+    mem_regions_count = 0
+    defines = {}
+    for option in options:
+        if not option.startswith('-D_'):
+            continue
+        parts = option.split('=', 1)
+        if len(parts) < 2:
+            continue
+        name = parts[0][3:]
+        value = parts[1]
+        defines[name] = value
+        if name.endswith('_OFF'):
+            mem_regions_count += 1
+    if mem_regions_count == 0:
+        return
+    f = open(file_name, 'w')
+    f.write('EXTERN(vector_table)\n')
+    f.write('ENTRY(reset_handler)\n')
+    f.write('\n')
+    f.write('MEMORY {\n')
+    for k, v in iteritems(defines):
+        if k.endswith('_OFF'):
+            continue
+        offset_key = '%s_OFF' % (k)
+        if not (offset_key in defines):
+            continue
+        offset = defines[offset_key]
+        length = v
+        if k.startswith('ROM'):
+            access = 'rx'
+        elif k == 'EEP':
+            access = 'r'
+        else:
+            access = 'rwx'
+        f.write('\t%s (%s) : ORIGIN = %s, LENGTH = %s\n' % (k.lower(), access, offset, length))
+    f.write('}\n')
+    f.write('\n')
+    f.write('SECTIONS {\n')
+    f.write('\t.text : {\n')
+    f.write('\t\t*(.vectors)\n')
+    f.write('\t\t*(.text)\n')
+    f.write('\t\t. = ALIGN(4);\n')
+    f.write('\t\t*(.rodata)\n')
+    f.write('\t\t. = ALIGN(4);\n')
+    f.write('\t} >rom\n')
+    f.write('\t.preinit_array : {\n')
+    f.write('\t\t__preinit_array_start = .;\n')
+    f.write('\t\tKEEP (*(.preinit_array))\n')
+    f.write('\t\t__preinit_array_end = .;\n')
+    f.write('\t} >rom\n')
+    f.write('\t.init_array : {\n')
+    f.write('\t\t__init_array_start = .;\n')
+    f.write('\t\tKEEP (*(SORT(.init_array.*)))\n')
+    f.write('\t\tKEEP (*(.init_array))\n')
+    f.write('\t\t__init_array_end = .;\n')
+    f.write('\t} >rom\n')
+    f.write('\t.fini_array : {\n')
+    f.write('\t\t__fini_array_start = .;\n')
+    f.write('\t\tKEEP (*(.fini_array))\n')
+    f.write('\t\tKEEP (*(SORT(.fini_array.*)))\n')
+    f.write('\t\t__fini_array_end = .;\n')
+    f.write('\t} >rom\n')
+    f.write('\t.ARM.extab : {\n')
+    f.write('\t\t*(.ARM.extab*)\n')
+    f.write('\t} >rom\n')
+    f.write('\t.ARM.exidx : {\n')
+    f.write('\t\t__exidx_start = .;\n')
+    f.write('\t\t*(.ARM.exidx*)\n')
+    f.write('\t\t__exidx_end = .;\n')
+    f.write('\t} >rom\n')
+    f.write('\t. = ALIGN(4);\n')
+    f.write('\t_etext = .;\n')
+    f.write('\t.data : {\n')
+    f.write('\t\t_data = .;\n')
+    f.write('\t\t*(.data*)\n')
+    f.write('\t\t. = ALIGN(4);\n')
+    f.write('\t\t_edata = .;\n')
+    f.write('\t} >ram AT >rom\n')
+    f.write('\t_data_loadaddr = LOADADDR(.data);\n')
+    f.write('\t.bss : {\n')
+    f.write('\t\t*(.bss*)\n')
+    f.write('\t\t*(COMMON)\n')
+    f.write('\t\t. = ALIGN(4);\n')
+    f.write('\t\t_ebss = .;\n')
+    f.write('\t} >ram\n')
+    for k, v in iteritems(defines):
+        if k.endswith('_OFF'):
+            continue
+        offset_key = '%s_OFF' % (k)
+        if not (offset_key in defines):
+            continue
+        if k in ('RAM', 'ROM'):
+            continue
+        name = k.lower()
+        f.write('\t.%s : {\n' % (name))
+        f.write('\t\t*(.%s*)\n' % (name))
+        f.write('\t\t. = ALIGN(4);\n')
+        f.write('\t} >%s\n' % (name))
+    f.write('\t/DISCARD/ : { *(.eh_frame) }\n')
+    f.write('\t. = ALIGN(4);\n')
+    f.write('\tend = .;\n')
+    f.write('}\n')
+    f.write('\n')
+    f.write('PROVIDE(_stack = ORIGIN(ram) + LENGTH(ram));\n')
+    f.close()
