96c0847292c9501fe3e9bd8062ee340f81fc6c66
Ivan Kolesnikov <kiv.apple@gmail.com>
2016-10-18 19:33:51 +0400

    Improve CMake toolchain

--- misc/toolchain.cmake
+++ misc/toolchain.cmake
@@ -8,9 +8,11 @@ else()
 	endif()
 endif()
 
-if(NOT DEFINED MCU_INFO_UTIL)
-	set(MCU_INFO_UTIL ${CMAKE_CURRENT_LIST_DIR}/../mcu-info-util.py) 
+find_program(MCU_INFO_UTIL mcu-info-util ${CMAKE_CURRENT_LIST_DIR}/..) 
+if (NOT MCU_INFO_UTIL)
+	message(FATAL_ERROR "mcu-info-util not found")
 endif()
+message(STATUS "mcu-info-util found at path ${MCU_INFO_UTIL}")
 
 execute_process(COMMAND ${MCU_INFO_UTIL} --mcu ${MCU} --find-prefix
 	OUTPUT_VARIABLE MCU_TOOLCHAIN_PREFIX
@@ -37,12 +39,6 @@ set(CMAKE_CXX_COMPILER ${MCU_COMPILER})
 set(UTIL_OBJCOPY ${MCU_TOOLCHAIN_PREFIX}objcopy)
 set(UTIL_SIZE ${MCU_TOOLCHAIN_PREFIX}size)
 
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${MCU_FLAGS}")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${MCU_FLAGS}")
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${MCU_FLAGS}")
-
-include_directories("${CMAKE_CURRENT_BINARY_DIR}")
-
 if((NOT TARGET mcu-header) AND ("${HEADER_FILE_NEED}" STREQUAL "yes"))
 	add_custom_command(OUTPUT mcudefs.h
 		COMMAND ${MCU_INFO_UTIL} ARGS --mcu ${MCU} ${MCU_INFO_UTIL_FLAGS} --header mcudefs.h
@@ -57,5 +53,9 @@ if((NOT TARGET linker-script) AND ("${LINKER_SCRIPT_NEED}" STREQUAL "yes"))
 		COMMENT "Generating linker script")
 	add_custom_target(linker-script ALL
 		DEPENDS script.ld)
-	set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Tscript.ld")
+	set(MCU_FLAGS "${MCU_FLAGS} -Tscript.ld")
 endif()
+
+add_definitions(${MCU_FLAGS})
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${MCU_FLAGS}" CACHE INTERNAL "")
+include_directories("${CMAKE_CURRENT_BINARY_DIR}")
