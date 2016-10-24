26d00ebb89cb54e9fa60040e47dc567bb6b8d69a
Ivan Kolesnikov <kiv.apple@gmail.com>
2016-10-24 19:53:41 +0400

    Quickfix

--- setup.py
+++ setup.py
@@ -8,23 +8,25 @@ package_dir = os.path.join(base_dir, 'mcu_info_util')
 data_files = []
 for root, dirs, files in os.walk(os.path.join(package_dir, 'data')):
     data_files += [os.path.join(root, file) for file in files]
-data_files = [os.path.relpath(data_file, package_dir) for data_file in data_files]
+package_data_files = [os.path.relpath(data_file, package_dir) for data_file in data_files]
+data_files = []
+for fname in ('rules.mk', 'toolchain.cmake'):
+    fullname = os.path.join(base_dir, 'misc', fname)
+    if os.path.isfile(fullname):
+        data_files.append(fullname)
 
 
 setup(
     name='mcu-info-util',
-    version='0.3',
+    version='0.4',
     author='Ivan Kolesnikov',
     author_email='kiv.apple@gmail.com',
     url='https://github.com/KivApple/mcu-info-util',
-    download_url='https://github.com/KivApple/mcu-info-util/archive/v0.3.tar.gz',
+    download_url='https://github.com/KivApple/mcu-info-util/archive/v0.4.tar.gz',
     packages=['mcu_info_util'],
     package_dir={'mcu_info_util': 'mcu_info_util'},
-    package_data={'mcu_info_util': data_files},
-    data_files=[('share/mcu-info-util', [
-        os.path.join(base_dir, 'misc', 'rules.mk'),
-        os.path.join(base_dir, 'misc', 'toolchain.cmake')
-    ])],
+    package_data={'mcu_info_util': package_data_files},
+    data_files=[('share/mcu-info-util', data_files)],
     entry_points={
         'console_scripts': [
             'mcu-info-util = mcu_info_util.__main__:main'
